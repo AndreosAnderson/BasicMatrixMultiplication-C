b9f64090425747ba20874a2134d5bdc2c41ba258
AndreosAnderson <andrzej.chwalenia@gmail.com>
2024-10-10 20:19:33 +0100

    Update matrix_ops.c

--- matrix_ops.c
+++ matrix_ops.c
@@ -1,4 +1,6 @@
 #include <stdlib.h>
+#include <windows.h>
+#include <psapi.h>
 
 void multiplyMatrix(double** m1, double** m2, double** result, int n) {
     for (int i = 0; i < n; i++) {
@@ -25,4 +27,27 @@ void freeMatrix(double** matrix, int n) {
         free(matrix[i]);
     }
     free(matrix);
+}
+
+void printMemoryAndCPUUsage() {
+    PROCESS_MEMORY_COUNTERS memCounter;
+    BOOL result = GetProcessMemoryInfo(GetCurrentProcess(), &memCounter, sizeof(memCounter));
+    if (result) {
+        printf("Memory usage: %ld KB\n", memCounter.WorkingSetSize / 1024);
+    }
+
+    FILETIME creationTime, exitTime, kernelTime, userTime;
+    if (GetProcessTimes(GetCurrentProcess(), &creationTime, &exitTime, &kernelTime, &userTime)) {
+        ULARGE_INTEGER kernel, user;
+        kernel.LowPart = kernelTime.dwLowDateTime;
+        kernel.HighPart = kernelTime.dwHighDateTime;
+        user.LowPart = userTime.dwLowDateTime;
+        user.HighPart = userTime.dwHighDateTime;
+
+        double system_cpu_time = (double)kernel.QuadPart / 1e7;  
+        double user_cpu_time = (double)user.QuadPart / 1e7;
+
+        printf("User CPU time: %f seconds\n", user_cpu_time);
+        printf("System CPU time: %f seconds\n", system_cpu_time);
+    }
 }
